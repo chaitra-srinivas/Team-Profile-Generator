083f63bf6ce505e414063ae75c7dfa83c684c5fa
Chaitra <chaitra.srinivas@gmail.com>
2021-10-22 22:02:56 +1100

    Added classes

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+// npm modules
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+// team
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const teamMembers = [];
+
+const getManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter Team Manager's name:",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter Team Manager's id:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter email:",
+      },
+      {
+        type: "number",
+        name: "officeNumber",
+        message: "Enter Manager's Office Number:",
+      },
+    ])
+    .then((teamManager) => {
+      const { id, name, email, officeNumber } = teamManager;
+      const Manager = new Manager(id, name, email, officeNumber);
+      teamMembers.push(Manager);
+      console.log(Manager);
+    })
+    .catch((err) => console.log(err));
+};
