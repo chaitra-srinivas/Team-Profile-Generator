51a119396a621268fc1b6474ac0b51adfe38d4ea
Chaitra <chaitra.srinivas@gmail.com>
2021-10-25 11:10:35 +1100

    Validations for input values

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const Employee = require("./lib/Employee");
+const { off } = require("process");
 
 const teamMembers = [];
 
@@ -19,21 +20,50 @@ const getManagerDetails = () => {
         type: "input",
         name: "name",
         message: "Enter Team Manager's name:",
+        validate: (name) => {
+          if (!name) {
+            return "Please enter Manager's name!";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "input",
         name: "id",
         message: "Enter Team Manager's id:",
+        validate: (id) => {
+          if (isNaN(id) || !id) {
+            return "Please enter a valid number";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "input",
         name: "email",
         message: "Enter email:",
+        validate: (email) => {
+          if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return true;
+          } else {
+            console.log("Please enter a valid email");
+            return false;
+          }
+        },
       },
       {
-        type: "number",
+        type: "input",
         name: "officeNumber",
         message: "Enter Manager's Office Number:",
+        validate: (officeNumber) => {
+          if (isNaN(officeNumber) || !officeNumber) {
+            return "Please enter a valid phone number";
+          } else {
+            return true;
+          }
+        },
       },
     ])
     .then((managerDetails) => {
@@ -61,28 +91,64 @@ const getEmployeeDetails = () => {
         type: "input",
         name: "id",
         message: "Enter the employee's id:",
+        validate: (id) => {
+          if (isNaN(id) || !id) {
+            return "Please enter a valid number";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "input",
         name: "name",
         message: "Enter the employee's name:",
+        validate: (name) => {
+          if (!name) {
+            return "Please enter Manager's name!";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "input",
         name: "email",
         message: "Enter the employee's email:",
+        validate: (email) => {
+          if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return true;
+          } else {
+            console.log("Please enter a valid email");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "github",
         when: (input) => input.role === "Engineer",
         message: "Enter Engineer's GitHub profile:",
+        validate: (github) => {
+          if (!github) {
+            return "Please enter a valid GitHub profile";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "input",
         name: "school",
         when: (input) => input.role === "Intern",
         message: "Enter Inter's School",
+        validate: (school) => {
+          if (!school) {
+            return "Please enter a school's name";
+          } else {
+            return true;
+          }
+        },
       },
       {
         type: "confirm",
